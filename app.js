c05688705dbf782ebf7ad41831858376ca544117
Devarajan M A <97373876+Devraaj0003@users.noreply.github.com>
2022-07-15 19:59:25 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+function show(){
+    var name = document.getElementById("name").value;
+    var pass = document.getElementById("pass").value;
+    document.writeln("username: "+name);
+    document.writeln("password: "+pass);
+    }
+
+    function forget(){
+        document.writeln("Try again later...")
+    }
